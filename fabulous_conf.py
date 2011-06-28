5db581e9f66b35276e58bf35840cb98ad0a03810
Jann Kleen <jann@pocketvillage.com>
2011-06-28 20:56:57 +0800

    home directories updated
    home directories now take the configured username into account

--- fabulous_conf.py
+++ fabulous_conf.py
@@ -29,7 +29,7 @@ fabconf['SSH_PRIVATE_KEY_PATH'] = '%s/%s' % (fabconf['SSH_PATH'], fabconf['EC2_K
 fabconf['PROJECT_NAME'] = "polls"
 
 # Where to install apps
-fabconf['APPS_DIR'] = "/home/ubuntu/webapps"
+fabconf['APPS_DIR'] = "/home/%s/webapps" % fabconf['SERVER_USERNAME']
 
 # Where you want your project installed: /APPS_DIR/PROJECT_NAME
 fabconf['PROJECT_PATH'] = "%s/%s" % (fabconf['APPS_DIR'], fabconf['PROJECT_NAME'])
@@ -38,7 +38,7 @@ fabconf['PROJECT_PATH'] = "%s/%s" % (fabconf['APPS_DIR'], fabconf['PROJECT_NAME'
 fabconf['DOMAINS'] = "example.com www.example.com"
 
 # Path for virtualenvs
-fabconf['VIRTUALENV_DIR'] = "/home/ubuntu/.virtualenvs"
+fabconf['VIRTUALENV_DIR'] = "/home/%s/.virtualenvs" % fabconf['SERVER_USERNAME']
 
 # Git username for the server
 fabconf['GIT_USERNAME'] = "Server"
@@ -56,7 +56,7 @@ fabconf['GITHUB_DEPLOY_KEY_PATH'] = "%s/%s" % (fabconf['SSH_PATH'], fabconf['GIT
 fabconf['GITHUB_REPO'] = "https://github.com/gcollazo/Blank-django-Project.git"
 
 # Virtualenv activate command
-fabconf['ACTIVATE'] = "source /home/ubuntu/.virtualenvs/%s/bin/activate" % fabconf['PROJECT_NAME']
+fabconf['ACTIVATE'] = "source /home/%s/.virtualenvs/%s/bin/activate" % (fabconf['SERVER_USERNAME'], fabconf['PROJECT_NAME'])
 
 # Name tag for your server instance on EC2
 fabconf['INSTANCE_NAME_TAG'] = "AppServer"
